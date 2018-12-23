fd62fc539a4c01eea52bcf4a71fc14556040b4ae
thomastrzos <trzos.tomasz@gmail.com>
2018-12-23 15:26:18 +0100

    Simplify validation in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   has_and_belongs_to_many :carts
 
-  validates :name, presence: true
-  validates :price, presence: true
+  validates_presence_of :name, :price
 end
