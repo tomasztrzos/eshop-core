338bcd4151ca8c96e5acda60c48a3da76fc12f54
thomastrzos <trzos.tomasz@gmail.com>
2019-01-08 21:37:53 +0100

    Rm some admin/products specs

--- spec/requests/admin/products_spec.rb
+++ spec/requests/admin/products_spec.rb
@@ -7,46 +7,6 @@ RSpec.describe 'Admin products API', type: :request do
 
   login_user
 
-  describe 'GET /api/v1/admin/products' do
-    before { get '/api/v1/admin/products', headers: { 'Authorization': access_token } }
-
-    it 'returns products' do
-      expect(json).not_to be_empty
-      expect(json.size).to eq(10)
-    end
-
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
-    end
-  end
-
-  describe 'GET /api/v1/admin/products/:id' do
-    before { get "/api/v1/admin/products/#{product_id}", headers: { 'Authorization': access_token } }
-
-    context 'when the record exists' do
-      it 'returns the product' do
-        expect(json).not_to be_empty
-        expect(json['id']).to eq(product_id.to_s)
-      end
-
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
-      end
-    end
-
-    context 'when the record does not exist' do
-      let(:product_id) { 100 }
-
-      it 'returns status code 404' do
-        expect(response).to have_http_status(404)
-      end
-
-      it 'returns a not found message' do
-        expect(response.body).to match(/Couldn't find Product/)
-      end
-    end
-  end
-
   describe 'POST /api/v1/admin/products' do
     let(:valid_attributes) { { name: 'Lightweight down jacket', price: 39.99 } }
 
